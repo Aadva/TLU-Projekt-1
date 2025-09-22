d5df2e8679b6ac767645cc2464e96418a368d036
Mirko Aadva <aadva@users.noreply.github.com>
2025-09-22 13:34:37 +0300

    Lisatud node serveri path

--- www_2.js
+++ www_2.js
@@ -2,10 +2,14 @@ const http = require("http");
 const fs = require("fs");
 //laeme mooduli pأ¤ringu parsimiseks
 const url = require("url");
+//failitee haldamiseks mooduli
+const path = require("path");
 const dateEt = require("./src/dateTimeET");
 const textRef = "txt/vanasonad.txt";
 const pageHead = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Mirko Aadva, veebiprogrammeerimine</title>\n</head>\n<body>\n';
+const pageBanner = '<img src="vp_banner_2025_TA.jpg" alt="kursuse bأ¤nner">';
 const pageBody = '\t<h1>Mirko Aadva, veebiprogrammeerimine</h1>\n\t<p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna أœlikoolis</a> ning ei sislda tأµsiseltvأµetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, peatselt programmeerime.</p>\n\t<hr>';
+const pageLink = '\n\t<p>Vaata <a href="/vanasonad">vanasأµnasid</a>.</p>';
 const pageFoot = '\n</body>\n</html>';
 
 http.createServer(function(req, res){
@@ -17,8 +21,10 @@ http.createServer(function(req, res){
 	if(currentUrl.pathname === "/"){
 		res.writeHead(200, {"Content-type": "text/html"});
 		res.write(pageHead);
+		res.write(pageBanner);
 		res.write(pageBody);
 		res.write("\n\t<p>Tأ¤na on " + dateEt.weekDay() + " " + dateEt.fullDate() + ".</p>");
+		res.write(pageLink);
 		res.write(pageFoot);
 		return res.end();
 	}
@@ -28,6 +34,7 @@ http.createServer(function(req, res){
 		fs.readFile(textRef, "utf8", (err, data)=>{
 			if(err){
 				res.write(pageHead);
+				res.write(pageBanner);
 				res.write(pageBody);
 				res.write("\n\t<p>Tأ¤na on " + dateEt.weekDay() + " " + dateEt.fullDate() + ".</p>\n\t<p>Kahjuks tأ¤naseks أ¼htki vanasأµna vأ¤lja pakkuda pole!</p>");
 				res.write(pageFoot);
@@ -40,6 +47,7 @@ http.createServer(function(req, res){
 				}
 				folkWisdomOutput += "\n\t</ol>";
 				res.write(pageHead);
+				res.write(pageBanner);
 				res.write(pageBody);
 				res.write("\n\t<p>Tأ¤na on " + dateEt.weekDay() + " " + dateEt.fullDate() + ".</p>");
 				res.write("\n\t<h2>Valik Eesti vanasأµnu</h2>")
@@ -48,5 +56,22 @@ http.createServer(function(req, res){
 				return res.end();
 			}
 		});	
+	}
+	
+	else if(currentUrl.pathname === "/vp_banner_2025_TA.jpg"){
+		//liidame muidu kأ¤ttesaamatu piltide kausta meiue veebi failiteega
+		let bannerPath = path.join(__dirname, "images");
+		fs.readFile(bannerPath + currentUrl.pathname, (err, data)=>{
+			if(err){
+				throw(err);
+			} else {
+				res.writeHead(200, {"Content-type": "image/jpeg"});
+				res.end(data);
+			}
+		});
+	}
+	
+	else {
+		res.end("Viga 404, ei leia sellist lehte!");
 	}
 }).listen(5132);
